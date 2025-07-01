dbc234dc34732bfc17de69ebac8cdca2d66f0075
Burak Tüzel <buraktsoftware@gmail.com>
2025-07-01 13:07:38 +0300

    v0.21 Spike Controller Created

---
+++ Assets/_GameAssets/Scripts/Skills/SpikeController.cs
@@ -0,0 +1,42 @@
+using System;
+using Unity.Netcode;
+using UnityEngine;
+
+public class SpikeController : NetworkBehaviour
+{
+    [SerializeField] private Collider _spikeCollider;
+
+    public override void OnNetworkSpawn()
+    {
+        PlayerSkillController.OnTimerFinished += PlayerSkillController_OnTimerFinished;
+
+        if (IsOwner)
+        {
+            SetOwnerVisualsRpc();
+        }
+    }
+
+    private void PlayerSkillController_OnTimerFinished()
+    {
+        DestroyRpc();
+    }
+
+
+    [Rpc(SendTo.ClientsAndHost)]
+    private void DestroyRpc()
+    {
+        if (IsServer)
+        {
+            Destroy(gameObject);
+        }
+    }
+    [Rpc(SendTo.Owner)]
+    private void SetOwnerVisualsRpc()
+    {
+        _spikeCollider.enabled = false;
+    }
+    public override void OnNetworkDespawn()
+    {
+        PlayerSkillController.OnTimerFinished -= PlayerSkillController_OnTimerFinished;
+    }
+}
